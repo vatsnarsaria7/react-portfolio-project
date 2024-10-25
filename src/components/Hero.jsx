144bcb54589f37b140b665e21a80641b37113e37
vatsnarsaria7 <vnarsaria@ucsd.edu>
2024-10-24 19:14:34 -0700

    Updated portfolio, added roommate project

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,7 +27,7 @@ const Hero = () => {
               animate="visible"
               className="bg-gradient-to-r from-purple-300 via-slate-500 to-pink-500 bg-clip-text text-3xl tracking-tight text-transparent"
             >
-              Web Developer
+              Software Engineer
             </motion.span>
             <motion.p
               variants={container(1)}
--- src/constants/index.js
+++ src/constants/index.js
@@ -6,44 +6,22 @@ import rplProject from "../assets/projects/rpl-project.jpg";
 import portfolioProject from "../assets/projects/portfolioProject.jpg";
 import iotProject from "../assets/projects/iotProject.jpg";
 
-export const HERO_CONTENT = `I am a dedicated Computer Science student at the University of California, San Diego. I have gained hands-on experience in web development, research, and technical support through various roles and projects. My skills include a strong command of programming languages such as JavaScript, Python, and C++, as well as experience in frontend and backend development, data analysis, and problem-solving. I enjoy working both independently and as part of a team, contributing to meaningful projects that drive innovation and improve user experiences. With a passion for learning and a collaborative approach, I am always eager to tackle new challenges and expand my technical expertise.`;
+export const HERO_CONTENT =
+  "I am a committed Computer Science student at the University of California, San Diego, with hands-on experience in web development, research, and technical support. My expertise spans programming languages like JavaScript and Python, and I have a solid foundation in both frontend and backend development, data analysis, and creative problem-solving. I thrive in collaborative environments, actively contributing to projects that drive innovation and enhance user experiences. With a passion for continuous learning and a proactive approach, I am always excited to take on new challenges and expand my technical skill set.";
 
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
-// export const EXPERIENCES = [
-//   {
-//     year: "July 2024 - Present",
-//     role: "Full Stack Developer Intern",
-//     company: "We Care Insurance",
-//     description: `Collaboratively developed a web-based insurance management system, assisted with migrating code from javascript to react.js, and implemented new features to improve user experience.o	Utilized Java, Spring Boot, and JPA Hibernate for backend development.`,
-//     technologies: ["Javascript", "React.js", "Next.js", "Spring Boot", "JPA Hibernate"],
-//   },
-//   {
-//     year: "2022 - 2023",
-//     role: "Frontend Developer",
-//     company: "Adobe",
-//     description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-//     technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-//   },
-//   {
-//     year: "2021 - 2022",
-//     role: "Full Stack Developer",
-//     company: "Facebook",
-//     description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-//     technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-//   },
-//   {
-//     year: "2020 - 2021",
-//     role: "Software Engineer",
-//     company: "Paypal",
-//     description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-//     technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-//   },
-// ];
-
 export const EXPERIENCES = [
   {
-    year: "July 2024 - Present",
+    year: "January 2024 - Present",
+    role: "Research Assistant",
+    company: "ESB.AI Lab",
+    description:
+      "Collaborating with Professor Edwin Solares on developing an app for agricultural data collection, enabling farmers to capture images and gather metadata for machine learning models. Responsible for backend integration and data collection workflows to support app functionality.",
+    technologies: ["App Development", "Data Labeling", "Swift"],
+  },
+  {
+    year: "July 2024 - October 2024",
     role: "Web Developer Intern",
     company: "We Care Insurance",
     description: `Collaboratively developed a web-based insurance management system, contributing to the migration from JavaScript to React. Utilized Bootstrap and Axios for frontend development and API handling, while conducting unit and integration testing using JEST and Postman. Assisted in containerizing applications to streamline deployment with Docker and managed version control via Bitbucket, integrating CI/CD pipelines through GitHub Actions.`,
@@ -58,13 +36,6 @@ export const EXPERIENCES = [
     ],
   },
   {
-    year: "January 2024 - Present",
-    role: "Research Assistant",
-    company: "ESB.AI Lab",
-    description: `Collaborated with Professor Edwin Solares to analyze and label data for image recognition. Conducted research on drone-based target identification.`,
-    technologies: ["Image Recognition", "Data Labeling", "Drone Research"],
-  },
-  {
     year: "September 2023 - Present",
     role: "Field Technician",
     company: "IT Services UCSD",
@@ -77,13 +48,6 @@ export const EXPERIENCES = [
     company: "UCSD Computer Science Department",
     description: `Tutored CSE8A: Introduction to Python Programming. Guided students in mastering core programming concepts and held office hours for assistance.`,
     technologies: ["Python", "Tutoring", "Student Guidance"],
-  },
-  {
-    year: "November 2020 - April 2021",
-    role: "Student Developer Intern",
-    company: "Myan Pro Solution Ltd.",
-    description: `Scoped requirements, coordinated design, and conducted User Acceptance Testing. Assisted in developing an online pharmacy application.`,
-    technologies: ["User Acceptance Testing", "Software Development"],
   },
 ];
 
@@ -120,10 +84,16 @@ export const EXPERIENCES = [
 
 export const PROJECTS = [
   {
+    title: "Roommate Suite Management App",
+    image: project1,
+    description: `Collaborating on the design and development of a web app for UCSD students to manage shared living spaces and reduce conflicts. Features include profile management, task tracking, calendar integration, and a points-based system to encourage task completion.`,
+    technologies: ["React", "Next.js", "Firebase", "Tailwind CSS"],
+  },
+  {
     title: "Rocket Propulsion Lab - G-Class Model Rocket",
     image: rplProject,
-    description: `Designed and constructed a G-class model rocket. CAD development in SolidWorks, simulation setup, and avionics programming in C++.`,
-    technologies: ["SolidWorks", "C++", "Rocket Simulation"],
+    description: `Designed and constructed a G-class model rocket. CAD development in SolidWorks, simulation setup, and avionics programming`,
+    technologies: ["SolidWorks", "Rocket Simulation"],
   },
   {
     title: "IoT Parking System",
